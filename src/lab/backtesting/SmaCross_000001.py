99721c1cdf3ba72f85c2cc78691060d4423ab90c
Benjamin Li <842958037@qq.com>
2025-03-31 09:58:19 +0800

    add 000001 test

--- src/lab/backtesting/SmaCross_000001.py
+++ src/lab/backtesting/SmaCross_000001.py
@@ -26,11 +26,12 @@ class SmaCross(Strategy):
 data = ak.stock_zh_a_hist(symbol='000001', period='daily', start_date='20200101', end_date='20250330', adjust='qfq')
 data.rename(columns={'开盘': 'Open', '最高' : 'High', '最低' : 'Low', '收盘' : 'Close'}, inplace=True)
 
+print(data)
 bt = Backtest(data, SmaCross,
               cash=10000, commission=.002,
               exclusive_orders=True)
 
 output = bt.run()
-bt.plot()
-print(output)
-print(output['_trades'])
+# bt.plot()
+# print(output)
+output['_trades'].to_csv('./output/trades_output.csv', index=False)
