ca74d64c7f1dc59582d7f790e4dd046f628a6959
true2blue <842958037@qq.com>
2025-04-02 18:19:18 +0800

    init

--- tests/test_app.py
+++ tests/test_app.py
@@ -23,57 +23,9 @@ class TestApp(unittest.TestCase):
     def test_get_buy_volume(self):
         self.assertEqual(self.app.get_buy_volume(10.13), 200)
 
-    def test_run_no_trade(self):
-        ydls = Ydls()
-        self.app = App([ydls], config_file='config-test-no-trade.ini')
-        res_list = []
-        for index, row in self.data.iterrows():
-            row_df = pd.DataFrame([row])
-            res = self.app.run(row_df, timeStr='09:46:00')
-            if res['state'] != 'nodata':
-                print(res)
-                res_list.append(res)
-        self.assertEqual(len(res_list), 1)
-        self.assertEqual(res_list[0]['state'], 'success')
-        self.assertEqual(res_list[0]['message'], 'data match, no trade')
-        self.assertEqual(res_list[0]['data'][0].name, '600239')
-        self.app.close()
-
-    def test_run_no_trade_multiple(self):
-        ydls = Ydls()
-        ydls_concept = YdlsConcept()
-        self.app = App([ydls_concept], config_file='config-test-no-trade.ini')
-        res_list = []
-        for index, row in self.data.iterrows():
-            row_df = pd.DataFrame([row])
-            res = self.app.run(row_df, timeStr='09:46:00')
-            if res['state'] != 'nodata':
-                print(res)
-                res_list.append(res)
-        self.assertEqual(len(res_list), 0)
-        self.app.close()
-
-
-    def test_run_no_trade_multiple(self):
-        ydls = Ydls()
-        ydls_concept = YdlsConcept()
-        self.app = App([ydls, ydls_concept], config_file='config-test-no-trade.ini')
-        res_list = []
-        for index, row in self.data.iterrows():
-            row_df = pd.DataFrame([row])
-            res = self.app.run(row_df, timeStr='09:46:00')
-            if res['state'] != 'nodata':
-                print(res)
-                res_list.append(res)
-        self.assertEqual(len(res_list), 1)
-        self.assertEqual(res_list[0]['state'], 'success')
-        self.assertEqual(res_list[0]['message'], 'data match, no trade')
-        self.assertEqual(res_list[0]['data'][0].name, '600239')
-        self.app.close()
-
-    # def test_run_trade(self):
+    # def test_run_no_trade(self):
     #     ydls = Ydls()
-    #     self.app = App([ydls], 'config-test-trade.ini')
+    #     self.app = App([ydls], config_file='config-test-no-trade.ini')
     #     res_list = []
     #     for index, row in self.data.iterrows():
     #         row_df = pd.DataFrame([row])
@@ -82,12 +34,60 @@ class TestApp(unittest.TestCase):
     #             print(res)
     #             res_list.append(res)
     #     self.assertEqual(len(res_list), 1)
-    #     self.assertEqual(len(self.app.get_success_order()), 1)
-    #     self.assertEqual(self.app.get_success_order()[0], '300534')
     #     self.assertEqual(res_list[0]['state'], 'success')
-    #     self.assertEqual(res_list[0]['message'], 'trade success')
-    #     self.assertEqual(res_list[0]['data'][0], '300534')
+    #     self.assertEqual(res_list[0]['message'], 'data match, no trade')
+    #     self.assertEqual(res_list[0]['data'][0].name, '600239')
     #     self.app.close()
+
+    # def test_run_no_trade_multiple(self):
+    #     ydls = Ydls()
+    #     ydls_concept = YdlsConcept()
+    #     self.app = App([ydls_concept], config_file='config-test-no-trade.ini')
+    #     res_list = []
+    #     for index, row in self.data.iterrows():
+    #         row_df = pd.DataFrame([row])
+    #         res = self.app.run(row_df, timeStr='09:46:00')
+    #         if res['state'] != 'nodata':
+    #             print(res)
+    #             res_list.append(res)
+    #     self.assertEqual(len(res_list), 0)
+    #     self.app.close()
+
+
+    # def test_run_no_trade_multiple(self):
+    #     ydls = Ydls()
+    #     ydls_concept = YdlsConcept()
+    #     self.app = App([ydls, ydls_concept], config_file='config-test-no-trade.ini')
+    #     res_list = []
+    #     for index, row in self.data.iterrows():
+    #         row_df = pd.DataFrame([row])
+    #         res = self.app.run(row_df, timeStr='09:46:00')
+    #         if res['state'] != 'nodata':
+    #             print(res)
+    #             res_list.append(res)
+    #     self.assertEqual(len(res_list), 1)
+    #     self.assertEqual(res_list[0]['state'], 'success')
+    #     self.assertEqual(res_list[0]['message'], 'data match, no trade')
+    #     self.assertEqual(res_list[0]['data'][0].name, '600239')
+    #     self.app.close()
+
+    def test_run_trade(self):
+        ydls = Ydls()
+        self.app = App([ydls], 'config-test-trade.ini')
+        res_list = []
+        for index, row in self.data.iterrows():
+            row_df = pd.DataFrame([row])
+            res = self.app.run(row_df, timeStr='09:46:00')
+            if res['state'] != 'nodata':
+                print(res)
+                res_list.append(res)
+        self.assertEqual(len(res_list), 1)
+        self.assertEqual(len(self.app.get_success_order()), 1)
+        self.assertEqual(self.app.get_success_order()[0], '300534')
+        self.assertEqual(res_list[0]['state'], 'success')
+        self.assertEqual(res_list[0]['message'], 'trade success')
+        self.assertEqual(res_list[0]['data'][0], '600239')
+        self.app.close()
         
 if __name__ == '__main__':
     unittest.main()
