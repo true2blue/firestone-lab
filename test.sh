1e298dc1b855933e8f94df9036eed901cf783d0c
Benjamin Li <842958037@qq.com>
2025-04-14 16:43:35 +0800

    init

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 export FR_DB=firestone
 # python -m unittest tests/test_util.py
-# python -m unittest tests/test_ydls.py
+python -m unittest tests/test_ydls.py
 # python -m unittest tests/test_trade.py
 # python -m unittest tests/test_app.py
-python -m unittest tests/test_proxy.py
+# python -m unittest tests/test_proxy.py
--- tests/test_ydls.py
+++ tests/test_ydls.py
@@ -21,7 +21,27 @@ class TestApp(unittest.TestCase):
     def is_match_time(self, row, time):
         return row['时间'] == time
     
-    def test_job(self):
+    # def test_job(self):
+    #     res_df = None
+    #     status = False
+    #     expected_time = None
+    #     actual_time = None
+    #     for index, row in self.data.iterrows():
+    #         row_df = pd.DataFrame([row])
+    #         res_df, status = self.ydls.job(row_df)
+    #         if self.is_match_time(row, '09:46:33'):
+    #             print('Expected')
+    #             print(res_df)
+    #             expected_time = res_df.iloc[0]['时间']
+    #         if status:
+    #             print('Actual')
+    #             print(res_df)
+    #             actual_time = res_df.iloc[0]['时间']
+    #             break
+    #     self.assertEqual(expected_time, actual_time)           
+    #     self.assertTrue(status)
+
+    def test_job_concept(self):
         res_df = None
         status = False
         expected_time = None
@@ -40,22 +60,6 @@ class TestApp(unittest.TestCase):
                 break
         self.assertEqual(expected_time, actual_time)           
         self.assertTrue(status)
-
-    # def test_job_concept(self):
-    #     res_df = None
-    #     status = False
-    #     expected_time = None
-    #     actual_time = None
-    #     for index, row in self.data.iterrows():
-    #         row_df = pd.DataFrame([row])
-    #         res_df, status = self.ydlsConcept.job(row_df)
-    #         if status:
-    #             print('Actual')
-    #             print(res_df)
-    #             actual_time = res_df.iloc[0]['时间']
-    #             break
-    #     self.assertEqual(expected_time, actual_time)           
-    #     self.assertFalse(status)
 
 if __name__ == '__main__':
     unittest.main()
